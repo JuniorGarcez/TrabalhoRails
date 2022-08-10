b24c24e3033979674aa2dbaedebdc2be9cdf883b
AndersonJunior <garcez285@gmail.com>
2022-08-10 17:42:54 -0300

    Feito paginação e tradução da aplicação

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,20 +1,20 @@
 class ArticlesController < ApplicationController
-
   http_basic_authenticate_with name: "Admin", password: "root", except: [:index, :show]
 
   def destroy
      @article = Article.find(params[:id])
      @article.destroy
-
      redirect_to articles_path
    end
 
   def index
-    if params[:search]
-      @articles = Article.search(params[:search])
-    else
-      @articles = Article.all
-    end
+      @limit_page = 4
+      @articles_count = Article.count
+      @pages = (@articles_count % @limit_page == 0) ? (@articles_count / @limit_page) -1 : (@articles_count / @limit_page)
+      @page = params.fetch(:page, 0).to_i
+      @articles = Article.search(params[:search]).offset(@page * @limit_page).limit(@limit_page)
+      @page + 0 == 0 ? @artilce_ini = 1 : @artilce_ini = (@page * @limit_page) + 1
+      @page == @pages ? @article_fim = @articles_count : @article_fim = (@page + 1) * @limit_page
   end
 
   def show
