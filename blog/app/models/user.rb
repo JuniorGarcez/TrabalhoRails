35358d811d1642ae85842ef82ad44dc26d038fa6
AndersonJunior <garcez285@gmail.com>
2022-08-15 18:05:45 -0300

    Estilizações

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :articles
-  validates :name , presence: true, length: {minimum: 4},
-            uniqueness: { case_sensitive: false}
+  validates :mail, :name, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }
+  #validates :name, presence: true, length: {minimum: 4}, uniqueness: {case_sensitive: false}
+  before_save :downcase_fields
+
+  def downcase_fields
+    self.name.downcase!
+  end
+
 end
